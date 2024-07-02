8a425c486d91a7a0b4dcadbdaf5ec4e6ad322d36
Mafabi-Israel-Emmanuel <israelmaleza01@outlook.com>
2024-07-02 13:27:41 +0300

    Software Engineering, Javascript

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+    GLORY BE TO GOD,
+    Variables Lab,
+    By Israel Mafabi Emmanuel
+*/
+
+const companyName = "Mafabi";
+let mostProfitableNeighborhood = "Chelsea";
+let companyCeo = "Emmanuel Mafabi";
